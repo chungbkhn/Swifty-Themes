a442a9e942066f9e8016eb96ea9daec63ec04e20
Chung Duong <chunbkhn87@gmail.com>
2018-11-10 11:51:58 +0700

    add pod

---
+++ SwiftyTheme/SwiftyTheme/Theme/Theme.swift
@@ -0,0 +1,28 @@
+//
+//  Theme.swift
+//  SwiftyTheme
+//
+//  Created by David on 11/7/18.
+//  Copyright © 2018 Chung Duong. All rights reserved.
+//
+
+import Foundation
+
+import SwiftyJSON
+
+struct Theme {
+
+	let color1: UIColor
+	let color2: UIColor
+	let color3: UIColor
+
+	let backgroundColor1: UIColor
+	let backgroundColor2: UIColor
+	let backgroundColor3: UIColor
+
+//	init(jsonString: String) {
+//		let json = JSON(stringLiteral: jsonString)
+//
+//		
+//	}
+}
--- SwiftyTheme/SwiftyTheme/ViewController.swift
+++ SwiftyTheme/SwiftyTheme/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+	@IBOutlet weak var firstButton: UIButton!
+	@IBOutlet weak var secondButton: UIButton!
+	@IBOutlet weak var thirdButton: UIButton!
+	@IBOutlet weak var backgroundView: UIView!
+
+	override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+	@IBAction func firstButton_TouchUpInside(_ sender: UIButton) {
+	}
+	
+	@IBAction func secondButton_TouchUpInside(_ sender: UIButton) {
+	}
+	
+	@IBAction func thirdButton_TouchUpInside(_ sender: UIButton) {
+	}
 
 }
 
